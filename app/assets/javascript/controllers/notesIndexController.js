a82574f2b78adf8253ac8e3b739ba6eb48cc7c7d
Joel Zarrias Conchillo <Joel@MacBook-Air-de-Joel.local>
2017-04-26 00:32:43 +0200

    Added directives, custom filters and category service

--- app/assets/javascript/controllers/notesIndexController.js
+++ app/assets/javascript/controllers/notesIndexController.js
@@ -1,4 +1,5 @@
 angular.module('NoteWrangler').controller('NotesIndexController', function(Note, $scope){
 	$scope.notes = Note.query();
-	console.log($scope.notes);
+	$scope.search = {};
+	window.s = $scope;
 });
